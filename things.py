54bfd2b1dc3536da739ceee3ac46c5468a680d8d
SaraKJ25BCE <sara.kj2025@vitstudent.ac.in>
2025-12-16 22:14:50 +0530

    Update things.py

--- things.py
+++ things.py
@@ -1,22 +1,5 @@
 from conf import *
 
-class Star:
-    def __init__(self):
-        self.p = (0,0)
-        self.color = STAR_COLOR
-        self.size = 0
-
-
-class Planet:
-    def __init__(self):
-        self.p = (0,0)
-        self.color = (0,0,0)
-        self.size = 0
-        self.ring = False
-        self.ring_density = 0
-        self.ring_tilt = 0
-
-
 class Meteor:
     def __init__(self):
         self.p = (0,0)
@@ -24,7 +7,6 @@ class Meteor:
         self.size = 0
         self.vertices_num = 3
         self.vertices = []
-        self.vertices_shade = []
         self.spin = ANGULAR_VELOCITY_METEOR
 
 
